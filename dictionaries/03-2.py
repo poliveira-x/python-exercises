6b56ff2b7635f5bc4f62591c272d8d94874ba73b
root <root@localhost.localdomain>
2023-07-04 02:23:46 +0000

    new way to solve the problem 03 added

--- dictionaries/03-2.py
+++ dictionaries/03-2.py
@@ -1,24 +1,24 @@
 # Exercise 3: Medium
 # Write a program that counts the frequency of each character in a given string and stores the results in a dictionary.
-# Ignore white space and punctuation marks. Print the
-# dictionary.
+# Ignore white space and punctuation marks. Print the dictionary.
+#
 #
 
 letters = 0
 others = 0
 string = {}
-for i in range(97, 123):
-    string[chr(i)] = 0 
 
 text = input("\nType a sentence: ").lower()
 
 for i in text:
-    if ord(i) < 97 or ord(i) > 122:
-        others += 1
-        pass
-    else:
-        string[i] = string[i] + 1
+    if not i in string and (ord(i) > 96 and ord(i) < 123): 
+        string[i] = 1
         letters += 1
+    elif i in string:
+        string[i] += 1
+        letters += 1
+    else:
+        others += 1
 
 for i in string:
     if string[i] > 0:
